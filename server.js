e3fa06cb4199bffce1bb3d860eff270d61d5334f
David Heyman <dave@axismaps.com>
2015-02-11 13:31:24 +0000

    Simple starter files

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require( 'express' ),
+	bodyParser = require( 'body-parser' ),
+    collect = require( './server/collect' );
+
+var app = express();
+
+app.use( function( req, res, next )
+{
+
+    // Website you wish to allow to connect
+    res.setHeader( 'Access-Control-Allow-Origin', '*' );
+
+    // Request methods you wish to allow
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use( function(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+});
+
+app.use( function(err, req, res, next) {
+  if (req.xhr) {
+    res.send(500, { error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+});
+
+app.use( function(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+});
+
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: 10000000}));
+
+app.post( '/add', meta.save );
+
+app.listen( 3000 );
+console.log( 'Listening on port 3000...' );
